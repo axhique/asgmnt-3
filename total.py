28b0fcf74d1df3f744921faf812795122389f949
axhique <124482531+axhique@users.noreply.github.com>
2023-02-19 13:46:01 +0530

    Add files via upload

---
+++ total.py
@@ -0,0 +1,20 @@
+def sum():
+    a=input("Enter the list :").split(",")
+    l1=[]
+    for i in a:
+        l1.append(int(i))
+    
+    sum=0
+    for j in l1:
+        sum +=j
+     
+    print(" + ".join(a),"=",sum)
+ 
+print(sum())
+
+
+# sample input : 
+
+#  Enter the list :5,6,4,8,5,444,89
+#  5 + 6 + 4 + 8 + 5 + 444 + 89 = 561
+ 
